e12700627c8010bbeadda29eb87d28f7354e8f04
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-07-15 20:34:42 +0300

    Hamburger içerikleri büyütülecek.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,13 +12,15 @@ public class PlayerController : MonoBehaviour
     public BGCcMath BGCcMath;
     public Transform ModelTransform;
     [Range(0, 10)]
-    public int TurnSpeed;
+    public int TurnSpeed, zSpeed;
+
+    public int fowSpeed = 10;
     public Animator animator;
-    public Camera cam;
+    public Camera cam1, cam2;
     public GameObject Bun2;
     public Collector collector;
     public GameObject JumpEffect;
-    bool isStarted;
+    bool isStarted, isGameOver;
 
     private void Start()
     {
@@ -27,24 +29,25 @@ public class PlayerController : MonoBehaviour
     }
 
     bool isButtonUp;
-    float count = 1;
+    float count = 1f;
     void Update()
     {
-        if (!isStarted)
+        if (!isStarted || isGameOver)
         {
             return;
         }
         //increase distance
-        distance += 5 * Time.deltaTime;
+        distance += zSpeed * Time.deltaTime;
 
         transform.position = BGCcMath.CalcPositionByDistance(distance);
 
         if (Input.GetMouseButton(0))
         {
             ModelTransform.Rotate(100 * TurnSpeed * Time.deltaTime, 0, 0);
-            if (cam.fieldOfView < 68)
+            if (cam1.fieldOfView < 68)
             {
-                cam.fieldOfView += Time.deltaTime * 10;
+                cam1.fieldOfView += Time.deltaTime * fowSpeed;
+                cam2.fieldOfView += Time.deltaTime * fowSpeed;
             }
         }
         if (Input.GetMouseButtonDown(0))
@@ -69,9 +72,10 @@ public class PlayerController : MonoBehaviour
             count = 1;
         }
 
-        if (isButtonUp && cam.fieldOfView > 60)
+        if (isButtonUp && cam1.fieldOfView > 60)
         {
-            cam.fieldOfView -= Time.deltaTime * 10;
+            cam1.fieldOfView -= Time.deltaTime * fowSpeed;
+            cam2.fieldOfView -= Time.deltaTime * fowSpeed;
         }
     }
 
@@ -80,15 +84,24 @@ public class PlayerController : MonoBehaviour
     {
         if (other.CompareTag("Trampoline"))
         {
-            //TODO trampoline animation
-            //TODO closing burger animation
+            //if (isButtonUp)
+            //{
             other.GetComponent<Animator>().SetTrigger("Jump");
             Instantiate(JumpEffect, new Vector3(other.transform.position.x, .75f, other.transform.position.z), Quaternion.identity);
             collector.CloseAllSoftBodies();
             animator.SetTrigger("CloseBun");
+            //}
+            //else
+            //{
+            //    //GameOver
+            //    Debug.Log("Game Over");
+            //    isGameOver = true;
+            //}
         }
         else if (other.CompareTag("FinishLine"))
         {
+            isStarted = false;
+            isGameOver = true;
             ModelTransform.DORotate(Vector3.zero, 1);
             animator.SetBool("isTurning", false);
             animator.SetBool("isFinish", true);
